4b647be2097ab83d00e916c9dee6d3ee154f18c6
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-01-04 10:45:39 +0100

    finished lesson 21

--- 30days/src/redux/configureStore.js
+++ 30days/src/redux/configureStore.js
@@ -1,13 +1,37 @@
-import {createStore, bindActionCreators, combineReducers} from 'redux';
+import {createStore, bindActionCreators, combineReducers, applyMiddleware} from 'redux';
 import * as currentTime from './modules/currentTime';
 import * as currentUser from './modules/currentUser';
+import 'whatwg-fetch';
+
+const apiMiddleware = (store) => (next) => (action) => {
+  if (!action.meta || action.meta.type !== 'api') {
+    return next(action);
+  }
+
+  const {url} = action.meta;
+  const fetchOptions = Object.assign({}, action.meta);
+
+  fetch(url, fetchOptions)
+    .then(resp => resp.json())
+    .then(json => {
+       if (typeof action.meta.onSuccess === 'function') {
+        action.meta.onSuccess(json);
+      }
+      return json; // For the next promise in the chain
+    })
+    .then(json => {
+      let newAction = Object.assign({}, action, {payload: json});
+      delete newAction.meta;
+      store.dispatch(newAction);
+    });
+};
 
 export const configureStore = () => {
   const reducer = combineReducers({
     currentTime: currentTime.reducer,
     currentUser: currentUser.reducer
   });
-  const store = createStore(reducer);
+  const store = createStore(reducer, applyMiddleware(apiMiddleware));
 
   const actions = {
     currentTime: bindActionCreators(currentTime.actions, store.dispatch),
