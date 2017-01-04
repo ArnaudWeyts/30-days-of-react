5dea985557e01bcf0b1cdb74ff580297afc6df90
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-01-04 12:27:03 +0100

    lmao this tutorial for tests sucked

--- 30days/src/components/Nav/__tests__/Navbar-test.js
+++ 30days/src/components/Nav/__tests__/Navbar-test.js
@@ -1,9 +1,13 @@
-describe('Navbar', () => {
-  it('passing test', () => {
-    expect(true).toBeTruthy();
-  });
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import {shallow} from 'enzyme';
 
-  it('failing test', () => {
-    expect(false).toBeFalsy();
+jest.unmock('../Navbar');
+import Navbar from '../Navbar';
+
+describe('Navbar', () => {
+  it('wraps content in a div with .navbar class', () => {
+    const navbar = shallow(<Navbar />);
+    expect(navbar).toBeDefined();
   });
 });
